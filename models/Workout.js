0d90e53752847e31574dc5fc73164a85f682eb94
Nitin Mukesh <nitinmukes@gmail.com>
2020-09-03 15:11:23 +0800

    Configured eslint and fixed formatting issues around files

---
+++ models/Workout.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+  type: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  distance: {
+    type: mongoose.Decimal128,
+    required: [
+      function() {
+        return this.type === "cardio";
+      },
+      "distance is required for cardio type excercise"
+    ]
+  },
+  sets: {
+    type: Number
+  },
+  reps: {
+    type: Number
+  }
+});
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [ExerciseSchema]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
